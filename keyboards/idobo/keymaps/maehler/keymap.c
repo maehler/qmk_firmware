96d01028fc36be85ac64befc7463863f00f54360
Niklas Mähler <niklas.mahler@gmail.com>
2020-12-01 19:10:15 +0100

    XD75 fine tuning

--- keyboards/idobo/keymaps/maehler/keymap.c
+++ keyboards/idobo/keymaps/maehler/keymap.c
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT_ortho_5x15(
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    GO_GAME, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    ESC_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_DEL,  KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
+    ESC_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_INS,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENT_SFT, \
     _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, _______  \
  ),
@@ -81,15 +81,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_FUNCTION] = LAYOUT_ortho_5x15(
     _______, _______, _______, _______, _______, _______, KC_NLCK, KC_SLSH, KC_ASTR, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, KC_UP,   _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, \
-    _______,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, _______, _______, _______, _______, \
+    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, LOWER,   _______, KC_P0,   _______, KC_PDOT, KC_PENT, RAISE,   _______, _______, _______, KC_MPLY  \
  ),
 
  [_LOWER] = LAYOUT_ortho_5x15(
     _______, _______, _______, _______, _______, _______, KC_NLCK, KC_SLSH, KC_ASTR, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_LPRN, KC_RPRN, _______, \
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_P4,   KC_P5,   KC_P6,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_P7,   KC_P8,   KC_P9,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_P4,   KC_P5,   KC_P6,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_P1,   KC_P2,   KC_P3,   KC_F12,  KC_TILD, KC_LT,   KC_GT,   _______, _______, \
     _______, _______, _______, _______, LOWER,   _______, KC_P0,   _______, KC_PDOT, KC_PENT, RAISE,   _______, _______, _______, KC_MPLY  \
  ),
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_RAISE] = LAYOUT_ortho_5x15(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, _______, KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, _______, KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, _______, KC_F12,  _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, LOWER,   KC_DEL,  _______, _______, KC_PDOT, _______, RAISE,   _______, _______, _______, KC_MPLY  \
  ),
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, SPC_F,   SPC_F,   _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______  \
+    KC_LCTL, _______, _______, KC_LALT, KC_SPC,  SPC_F,   _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______  \
  ),
 
  [_GAMING_FUNC] = LAYOUT_ortho_5x15(
